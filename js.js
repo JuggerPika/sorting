3a29887b9e273263c51132183762643c342bc530
JuggerPika <58883774+JuggerPika@users.noreply.github.com>
2021-02-21 12:17:50 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+const months = ['August', 'February', 'April', 'October'];
+months.sort();
+console.log(months);
+
+
+let numbers = [2, 4, 1, 5, 3, ];
+numbers.sort((a, b) => a - b);
+console.log(numbers);
+
+
+const array1 = [30, 1, 21, 4, 100000];
+array1.sort();
+console.log(array1);
